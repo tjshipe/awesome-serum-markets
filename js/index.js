70acbd0cc578bdbb2b59f61798a29a67fca50f0b
dr497 <anand.ratiney@gmail.com>
2020-11-26 15:28:14 +0800

    Add tokens

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const solana = require("@solana/web3.js");
 const Markets = require("./markets.json");
+const Tokens = require("./tokens.json");
 
 const AWESOME_MARKETS = Markets.map(market => {
   return {
@@ -10,4 +11,14 @@ const AWESOME_MARKETS = Markets.map(market => {
   };
 });
 
-module.exports = AWESOME_MARKETS;
+const AWESOME_TOKENS = Tokens.map(token => {
+  return {
+    name: token.name,
+    address: new solana.PublicKey(token.address)
+  };
+});
+
+module.exports = {
+  AWESOME_MARKETS: AWESOME_MARKETS,
+  AWESOME_TOKENS: AWESOME_TOKENS
+};
